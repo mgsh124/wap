717fa8b5c69fc7c32d31a2310dca345dd1a0fe85
Huy Luong <mgsh124@gmail.com>
2019-05-16 17:53:27 -0500

    Add jquery quiz 1 and mouse maze lab

---
+++ labs/mouse-maze/maze.js
@@ -0,0 +1,52 @@
+/* global jQuery */
+/* jshint esversion:6 */
+(function ($) {
+    "use strict";
+
+    let hits = 0;
+    let started = false;
+    $(document).ready(function () {
+        $("body").on("mouseover", ".boundary", function () {
+            if (!started) { return; }
+            displayLose();
+            hits++;
+        });
+
+        $("#maze").on("mouseleave", function () {
+            if (started) {
+                displayLose();
+            }
+        });
+
+        $("#start").on("click", function () {
+            startGame();
+        });
+
+        $("#end").on("mouseenter", function () {
+            endGame();
+        });
+
+        const displayLose = () => {
+            $(".boundary").not(".example").addClass("youlose");
+            $("#status").text("Sorry! You lose!");
+        };
+
+        const startGame = () => {
+            hits = 0;
+            started = true;
+            $("#status").text("Begin!");
+            $(".boundary").not(".example").removeClass("youlose");
+        };
+
+        const endGame = () => {
+            if (started) {
+                if (hits > 0) {
+                    $("#status").text("Sorry! You lose!");
+                } else {
+                    $("#status").text("Well done! You win!");
+                }
+                started = false;
+            }
+        };
+    });
+})(jQuery);
